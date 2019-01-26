c502b514243b6e3f8d7738b1372841ac047976e6
Ben Ayers-Glassey <bayersglassey@gmail.com>
2019-01-26 10:37:10 -0800

    bigger game screen. Added Entity class, array of entities (but only fly for now....)

--- game.js
+++ game.js
@@ -12,17 +12,92 @@ var kdown = {};
 
 var tick = 0;
 
-var trails = [];
-var max_n_trails = 20;
+var entities = [];
+function Entity(options){
+    this.trails = [];
+    this.trail_ratio = .4; /* So like... if 1, trail will be unbroken. If 0, trail will be dots. */
+    this.max_n_trails = 20;
 
-var x = 10;
-var y = 10;
-var vx = 0;
-var vy = 0;
+    this.x = canvas.width / 2;
+    this.y = canvas.height / 2;
+    this.vx = 0;
+    this.vy = 0;
 
-var accel = .5;
-var damp = .95;
-var fly_radius = 10;
+    this.accel = .5;
+    this.damp = .95;
+    this.bounce = .5;
+    this.radius = 10;
+    this.color = 'green';
+
+    /* Caller can override default attributes */
+    for(key in options){
+        this[key] = options[key];
+    }
+
+    /* Push onto global array of entities */
+    entities.push(this);
+}
+Entity.prototype = {
+    step: function(){
+        if(tick % 3 === 0)this.trails.push({x:this.x, y:this.y});
+        if(this.trails.length > this.max_n_trails)this.trails.shift();
+
+        if(kdown[KUP])this.vy-=this.accel;
+        if(kdown[KDOWN])this.vy+=this.accel;
+        if(kdown[KLEFT])this.vx-=this.accel;
+        if(kdown[KRIGHT])this.vx+=this.accel;
+
+        this.x += this.vx;
+        this.y += this.vy;
+        if(this.x < 0){
+            this.x = 0;
+            this.vx *= -this.bounce;
+        }else if(this.x >= canvas.width){
+            this.x = canvas.width - 1;
+            this.vx *= -this.bounce;
+        }
+        if(this.y < 0){
+            this.y = 0;
+            this.vy *= -this.bounce;
+        }else if(this.y >= canvas.height){
+            this.y = canvas.height - 1;
+            this.vy *= -this.bounce;
+        }
+
+        this.vx *= this.damp;
+        this.vy *= this.damp;
+    },
+    render: function(){
+        var ctx = canvas.getContext('2d');
+
+        /* Clear screen */
+        ctx.fillStyle = 'white';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        /* Render trails */
+        for(var i = 0; i < this.trails.length - 1; i++){
+            var trail = this.trails[i];
+            var next_trail = this.trails[i+1];
+            var mul = this.trail_ratio;
+            var addx = (next_trail.x - trail.x) * mul;
+            var addy = (next_trail.y - trail.y) * mul;
+
+            ctx.strokeStyle = 'cyan';
+            ctx.beginPath();
+            ctx.moveTo(trail.x, trail.y);
+            ctx.lineTo(trail.x + addx, trail.y + addy);
+            ctx.stroke();
+        }
+
+        /* Render a ...fly */
+        ctx.strokeStyle = 'green';
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+        ctx.stroke();
+    },
+}
+
+var fly = new Entity();
 
 function init(){
     $(document).on('keydown', keydown);
@@ -33,65 +108,18 @@ function init(){
 
 function step(){
     tick++;
-
-    if(tick % 3 === 0)trails.push({x:x, y:y});
-    if(trails.length > max_n_trails)trails.shift();
-
-    if(kdown[KUP])vy-=accel;
-    if(kdown[KDOWN])vy+=accel;
-    if(kdown[KLEFT])vx-=accel;
-    if(kdown[KRIGHT])vx+=accel;
-
-    x += vx;
-    y += vy;
-    if(x < 0){
-        x = 0;
-        vx *= -.5;
-    }else if(x >= canvas.width){
-        x = canvas.width - 1;
-        vx *= -.5;
+    for(var i = 0; i < entities.length; i++){
+        var entity = entities[i];
+        entity.step();
     }
-    if(y < 0){
-        y = 0;
-        vy *= -.5;
-    }else if(y >= canvas.height){
-        y = canvas.height - 1;
-        vy *= -.5;
-    }
-
-    vx *= damp;
-    vy *= damp;
-
     render();
 }
 
 function render(){
-    var ctx = canvas.getContext('2d');
-
-    /* Clear screen */
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    /* Render trails */
-    for(var i = 0; i < trails.length - 1; i++){
-        var trail = trails[i];
-        var next_trail = trails[i+1];
-        var mul = .4;
-        var addx = (next_trail.x - trail.x) * mul;
-        var addy = (next_trail.y - trail.y) * mul;
-
-        ctx.strokeStyle = 'cyan';
-        ctx.beginPath();
-        ctx.moveTo(trail.x, trail.y);
-        ctx.lineTo(trail.x + addx, trail.y + addy);
-        ctx.stroke();
+    for(var i = 0; i < entities.length; i++){
+        var entity = entities[i];
+        entity.render();
     }
-
-    /* Render a ...fly */
-    ctx.strokeStyle = 'green';
-    ctx.beginPath();
-    ctx.arc(x, y, fly_radius, 0, 2 * Math.PI);
-    ctx.stroke();
 }
 
 function keydown(event){
@@ -104,14 +132,14 @@ function keyup(event){
 }
 
 function click(event){
-    console.log(event);
+    //console.log(event);
     var target_x = event.offsetX;
     var target_y = event.offsetY;
     var mul = .1;
-    var addx = (target_x - x) * mul;
-    var addy = (target_y - y) * mul;
-    vx += addx;
-    vy += addy;
+    var addx = (target_x - fly.x) * mul;
+    var addy = (target_y - fly.y) * mul;
+    fly.vx += addx;
+    fly.vy += addy;
 }
 
 
